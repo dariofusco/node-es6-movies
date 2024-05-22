e4e22dcf95180ff0da7f53b11dc41bb56b6ef00f
dariofusco <fdario@hotmail.it>
2024-05-22 14:53:58 +0200

    crea classe tvserie

--- movie.js
+++ movie.js
@@ -1,3 +1,5 @@
+const { reset } = require("nodemon");
+
 const movies = [
     {
         title: "",
@@ -10,7 +12,7 @@ const movies = [
 
 class Movie {
     constructor(title, year, genre, rating, type) {
-        this.title = title,
+            this.title = title,
             this.year = year,
             this.genre = genre,
             this.rating = rating,
@@ -20,3 +22,13 @@ class Movie {
 
 const movie = new Movie("Jaws", 1975, "Drama", 8, "film");
 console.log(movie);
+
+class TvSerie extends Movie {
+    constructor(title, year, genre, rating, type, seasons) {
+            super(title, year, genre, rating, type),
+            this.seasons = seasons
+    }
+}
+
+const tvserie = new TvSerie("Breaking Bad", 2008, "Drama", 9.5, "serie tv", 5);
+console.log(tvserie);
